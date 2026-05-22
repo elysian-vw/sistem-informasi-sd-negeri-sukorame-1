3f4dbb00736ab4dc3b9eb9c62661f7b9535eee32
Madiennasaa <ahmadubai02@gmail.com>
2026-05-22 09:17:31 +0700

    debugging auth

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,7 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'role' => 'siswa',
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
@@ -39,7 +39,7 @@ class UserFactory extends Factory
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
+            // No-op since email verification is not used in this project
         ]);
     }
 }
